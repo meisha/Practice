c48e0dd835436b93446e843ad002c55c7a8be3af
Meisha Ray <meisharay@aol.com>
2012-10-18 12:50:52 -0400

    such crap

--- MovieTrailerP4/MovieTrailerP4/MMAViewController.m
+++ MovieTrailerP4/MovieTrailerP4/MMAViewController.m
@@ -54,91 +54,91 @@
     
     
     
-    Movie *movie1 = [Movie new];
-    movie1.movieName = @"The Lorax";
-    movie1.times = @"2:40pm, 5:40pm, 7:40pm";
-    movie1.imageFile = @"theLorax.jpg";
-    
-    
-    Movie *movie2 = [Movie new];
-    movie2.movieName = @"G.I. Joe";
-    movie2.times = @"2:40pm, 5:40pm, 7:40pm";
-    movie2.imageFile = @"mushroom_risotto.jpg";
-    
-    
-    Movie *movie3 = [Movie new];
-    movie3.movieName = @"Safe";
-    movie3.times = @"2:40pm, 5:40pm, 6:00pm";
-    movie3.imageFile = @"safe.jpg";
-    
-    
-    Movie *movie4 = [Movie new];
-    movie4.movieName = @"Battleship";
-    movie4.times = @"2:40pm, 5:40pm, 7:40pm";
-    movie4.imageFile = @"battleship.jpg";
-    
-    
-    Movie *movie5 = [Movie new];
-    movie5.movieName = @"Finding Nemo 3D";
-    movie5.times = @"1:30pm, 6:40pm, 9:40pm";
-    movie5.imageFile = @"nemo.jpg";
-    
-    Movie *movie6 = [Movie new];
-    movie6.movieName = @"The Lorax";
-    movie6.times = @"2:40pm, 5:40pm, 7:40pm";
-    movie6.imageFile = @"theLorax.jpg";
-    
-    
-    Movie *movie7 = [Movie new];
-    movie7.movieName = @"G.I. Joe";
-    movie7.times = @"2:40pm, 5:40pm, 7:40pm";
-    movie7.imageFile = @"mushroom_risotto.jpg";
-    
-    
-    Movie *movie8 = [Movie new];
-    movie8.movieName = @"Safe";
-    movie8.times = @"2:40pm, 5:40pm, 6:00pm";
-    movie8.imageFile = @"safe.jpg";
-    
-    
-    Movie *movie9 = [Movie new];
-    movie9.movieName = @"Battleship";
-    movie9.times = @"2:40pm, 5:40pm, 7:40pm";
-    movie9.imageFile = @"battleship.jpg";
-    
-    
-    Movie *movie10 = [Movie new];
-    movie10.movieName = @"Finding Nemo 3D";
-    movie10.times = @"1:30pm, 6:40pm, 9:40pm";
-    movie10.imageFile = @"nemo.jpg";
-    
-    Movie *movie11 = [Movie new];
-    movie11.movieName = @"Finding Nemo 3D";
-    movie11.times = @"1:30pm, 6:40pm, 9:40pm";
-    movie11.imageFile = @"nemo.jpg";
-    
-    Movie *movie12 = [Movie new];
-    movie12.movieName = @"Finding Nemo 3D";
-    movie12.times = @"1:30pm, 6:40pm, 9:40pm";
-    movie12.imageFile = @"nemo.jpg";
-    
-    Movie *movie13 = [Movie new];
-    movie13.movieName = @"Finding Nemo 3D";
-    movie13.times = @"1:30pm, 6:40pm, 9:40pm";
-    movie13.imageFile = @"nemo.jpg";
-    
-    Movie *movie14 = [Movie new];
-    movie14.movieName = @"Finding Nemo 3D";
-    movie14.times = @"1:30pm, 6:40pm, 9:40pm";
-    movie14.imageFile = @"nemo.jpg";
-    
-    Movie *movie15 = [Movie new];
-    movie15.movieName = @"Finding Nemo 3D";
-    movie15.times = @"1:30pm, 6:40pm, 9:40pm";
-    movie15.imageFile = @"nemo.jpg";
-    
-    
-    movies = [NSArray arrayWithObjects:movie1, movie2, movie3, movie4, movie5, movie6, movie7, movie8, movie9, movie10, movie11, movie12, movie13, movie14, movie15, nil];
+//    Movie *movie1 = [Movie new];
+//    movie1.movieName = @"The Lorax";
+//    movie1.times = @"2:40pm, 5:40pm, 7:40pm";
+//    movie1.imageFile = @"theLorax.jpg";
+//    
+//    
+//    Movie *movie2 = [Movie new];
+//    movie2.movieName = @"G.I. Joe";
+//    movie2.times = @"2:40pm, 5:40pm, 7:40pm";
+//    movie2.imageFile = @"mushroom_risotto.jpg";
+//    
+//    
+//    Movie *movie3 = [Movie new];
+//    movie3.movieName = @"Safe";
+//    movie3.times = @"2:40pm, 5:40pm, 6:00pm";
+//    movie3.imageFile = @"safe.jpg";
+//    
+//    
+//    Movie *movie4 = [Movie new];
+//    movie4.movieName = @"Battleship";
+//    movie4.times = @"2:40pm, 5:40pm, 7:40pm";
+//    movie4.imageFile = @"battleship.jpg";
+//    
+//    
+//    Movie *movie5 = [Movie new];
+//    movie5.movieName = @"Finding Nemo 3D";
+//    movie5.times = @"1:30pm, 6:40pm, 9:40pm";
+//    movie5.imageFile = @"nemo.jpg";
+//    
+//    Movie *movie6 = [Movie new];
+//    movie6.movieName = @"The Lorax";
+//    movie6.times = @"2:40pm, 5:40pm, 7:40pm";
+//    movie6.imageFile = @"theLorax.jpg";
+//    
+//    
+//    Movie *movie7 = [Movie new];
+//    movie7.movieName = @"G.I. Joe";
+//    movie7.times = @"2:40pm, 5:40pm, 7:40pm";
+//    movie7.imageFile = @"mushroom_risotto.jpg";
+//    
+//    
+//    Movie *movie8 = [Movie new];
+//    movie8.movieName = @"Safe";
+//    movie8.times = @"2:40pm, 5:40pm, 6:00pm";
+//    movie8.imageFile = @"safe.jpg";
+//    
+//    
+//    Movie *movie9 = [Movie new];
+//    movie9.movieName = @"Battleship";
+//    movie9.times = @"2:40pm, 5:40pm, 7:40pm";
+//    movie9.imageFile = @"battleship.jpg";
+//    
+//    
+//    Movie *movie10 = [Movie new];
+//    movie10.movieName = @"Finding Nemo 3D";
+//    movie10.times = @"1:30pm, 6:40pm, 9:40pm";
+//    movie10.imageFile = @"nemo.jpg";
+//    
+//    Movie *movie11 = [Movie new];
+//    movie11.movieName = @"Finding Nemo 3D";
+//    movie11.times = @"1:30pm, 6:40pm, 9:40pm";
+//    movie11.imageFile = @"nemo.jpg";
+//    
+//    Movie *movie12 = [Movie new];
+//    movie12.movieName = @"Finding Nemo 3D";
+//    movie12.times = @"1:30pm, 6:40pm, 9:40pm";
+//    movie12.imageFile = @"nemo.jpg";
+//    
+//    Movie *movie13 = [Movie new];
+//    movie13.movieName = @"Finding Nemo 3D";
+//    movie13.times = @"1:30pm, 6:40pm, 9:40pm";
+//    movie13.imageFile = @"nemo.jpg";
+//    
+//    Movie *movie14 = [Movie new];
+//    movie14.movieName = @"Finding Nemo 3D";
+//    movie14.times = @"1:30pm, 6:40pm, 9:40pm";
+//    movie14.imageFile = @"nemo.jpg";
+//    
+//    Movie *movie15 = [Movie new];
+//    movie15.movieName = @"Finding Nemo 3D";
+//    movie15.times = @"1:30pm, 6:40pm, 9:40pm";
+//    movie15.imageFile = @"nemo.jpg";
+//    
+//    
+//    movies = [NSArray arrayWithObjects:movie1, movie2, movie3, movie4, movie5, movie6, movie7, movie8, movie9, movie10, movie11, movie12, movie13, movie14, movie15, nil];
 }
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
